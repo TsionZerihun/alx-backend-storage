a116a5e07bd204ef4c778bbcba452edf605ad251
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-03-27 16:51:08 +0300

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country CHAR(2) DEFAULT 'US' NOT NULL CHECK (country IN ('US', 'CO', 'TN'))
 );
