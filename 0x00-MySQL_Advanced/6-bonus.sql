6a8e3d23590eb71d73097ad85d081fd965bdb40d
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-03-27 20:25:10 +0300

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,24 @@
+-- AddBonus that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
+BEGIN
+    DECLARE count INT DEFAULT 10;
+    DECLARE procduce_id INT DEFAULT 0;
+
+    SELECT COUNT(id) 
+        INTO count 
+        FROM projects 
+        WHERE name = project_name;
+    IF count = 0 THEN 
+        INSERT INTO projects(name) 
+            VALUE(project_name);
+    END IF;
+    SELECT id 
+        INTO procduce_id 
+        FROM projects 
+        WHERE name = procduce_id;
+    INSERT INTO corrections(user_id,procduce_id,score)
+        VALUES (user_id,procduce_id,score);
+END $$
+DELIMITER ;
