059d30d2164ac97f1b2b0d707cff35edd5f8b267
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-03-27 18:20:08 +0300

    task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS sub;
+DELIMITER $$
+CREATE TRIGGER sub
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
