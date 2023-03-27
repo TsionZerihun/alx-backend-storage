3075bba2d53c7771b95fae3acf47e531d8b31ebe
TsionZerihun <TsionZelekeZerihun@gmail.com>
2023-03-27 16:42:14 +0300

    task1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- recreate user table with additional colomns
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
+    name VARCHAR(255),
+    country CHAR(2) DEFAULT 'US' NOT NULL CHECK (country IN ('US', 'CO', 'TN'))
+);
