c7c693206bf17d7680b07c8ded247658423ee994
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-03-27 12:11:02 +0300

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+--- create users table in hoberton DB
 DROP TABLE IF EXISTS users;
 CREATE TABLE
 users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR (255) UNIQUE NOT NULL, name VARCHAR (255));
