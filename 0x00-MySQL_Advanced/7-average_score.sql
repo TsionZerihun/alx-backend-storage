e47bcf9dd5c8a6510714305eec8e0443cb4cfbff
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-03-28 11:24:33 +0300

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,8 +3,7 @@ DELIMITER $$
 DROP procedure if EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT);
 BEGIN
-    UPDATE users SET average_score = (SELECT AVG(score) as avg FROM corrections WHERE user_id = user_id) WHERE id = user_id;
-    SELECT * FROM users;
-
-END$$
-DELIMITER;
+    UPDATE users SET average_score = (SELECT AVG(score) as avg FROM corrections WHERE user_id = user_id) 
+    WHERE id = user_id;
+END $$
+DELIMITER ;
